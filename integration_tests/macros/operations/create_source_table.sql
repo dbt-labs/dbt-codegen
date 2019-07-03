6cb94ccc1373f3092e9f4135ccf11aebb4c1daa4
Claire Carroll <carroll.claire.e@gmail.com>
2019-07-03 15:11:40 -0400

    Add lightweight integration tests

---
+++ integration_tests/macros/operations/create_source_table.sql
@@ -0,0 +1,24 @@
+{% macro create_source_table() %}
+
+{% set target_schema="codegen_integration_tests__data_source_schema" %}
+
+{% do adapter.create_schema(target.database, target_schema) %}
+
+{% set drop_table_sql %}
+drop table if exists {{ target_schema }}.codegen_integration_tests__data_source_table
+{% endset %}
+
+{{ run_query(drop_table_sql) }}
+
+
+{% set create_table_sql %}
+create table {{ target_schema }}.codegen_integration_tests__data_source_table as (
+    select
+        1 as my_integer_col,
+        true as my_bool_col
+)
+{% endset %}
+
+{{ run_query(create_table_sql) }}
+
+{% endmacro %}
---
+++ integration_tests/tests/test_generate_source.sql
@@ -0,0 +1,17 @@
+
+{% set raw_schema = generate_schema_name('raw_data') %}
+
+-- test default args
+{% set actual_source_yaml = codegen.generate_source(raw_schema) %}
+
+{% set expected_source_yaml %}
+version: 2
+
+sources:
+  - name: {{ raw_schema | trim }}
+    tables:
+      - name: data__a_relation
+{% endset %}
+
+
+{{ assert_equal (actual_source_yaml | trim, expected_source_yaml | trim) }}
