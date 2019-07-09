fce2b8c54799d7fc8124af6958bd8be02620c304
Claire Carroll <carroll.claire.e@gmail.com>
2019-07-09 12:59:17 -0400

    maaaybe?

--- integration_tests/macros/create_source_table.sql
+++ integration_tests/macros/create_source_table.sql
@@ -4,16 +4,21 @@
 
 {% do adapter.create_schema(target.database, target_schema) %}
 
-{% set query_sql %}
-drop table if exists {{ target_schema }}.codegen_integration_tests__data_source_table;
+{% set drop_table_sql %}
+drop table if exists {{ target_schema }}.codegen_integration_tests__data_source_table
+{% endset %}
+
+{{ run_query(drop_table_sql) }}
+
+
+{% set create_table_sql %}
 create table {{ target_schema }}.codegen_integration_tests__data_source_table as (
     select
         1 as my_integer_col,
         true as my_bool_col
 )
-
 {% endset %}
 
-{{ run_query(query_sql) }}
+{{ run_query(create_table_sql) }}
 
 {% endmacro %}
