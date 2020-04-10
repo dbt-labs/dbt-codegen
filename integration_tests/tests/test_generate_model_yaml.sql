a360617740adb87f8d1df3a89b4750dc22cd73d3
Jonathan Talmi <jonathan@snaptravel.com>
2020-04-09 21:24:48 -0400

    add generate model yaml macro

---
+++ integration_tests/tests/test_generate_model_yaml.sql
@@ -0,0 +1,23 @@
+
+{% set actual_model_yaml = codegen.generate_model_yaml(
+    model_name='codegen_integration_tests__data_source_table'
+  )
+%}
+
+{% set expected_model_yaml %}
+version: 2
+
+models:
+  - name: model_yaml
+    tables:
+      - name: codegen_integration_tests__data_source_table
+        descriptions: ""
+        columns:
+          - name: col_a
+            description: ""
+          - name: col_b
+            description: ""
+
+{% endset %}
+
+{{ assert_equal (actual_model_yaml | trim, expected_model_yaml | trim) }}
