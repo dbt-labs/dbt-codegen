00199e912d3b76374e963cd169c2bc77f60faea8
Josh Devlin <joshuadevlin@gmail.com>
2021-06-02 10:20:35 -0500

    Add optional leading_commas arg to generate_base_model (#41)
    * Add optional leading_commas arg
    
    This adds leading comma support by adding a `leading_commas` arg to the
    `generate_base_model` macro. If the arg is omitted, the macro behaves
    the same.  If `True`, the output uses leading rather than trailing
    commas.
    
    * Update changelog
    
    * Update CHANGELOG.md
    
    Co-authored-by: Claire Carroll <carroll.claire.e@gmail.com>

---
+++ integration_tests/tests/test_generate_base_models_leading.sql
@@ -0,0 +1,29 @@
+
+{% set actual_base_model = codegen.generate_base_model(
+    source_name='codegen_integration_tests__data_source_schema',
+    table_name='codegen_integration_tests__data_source_table',
+    leading_commas=True,
+  )
+%}
+
+{% set expected_base_model %}
+with source as (
+
+    select * from {%raw%}{{ source('codegen_integration_tests__data_source_schema', 'codegen_integration_tests__data_source_table') }}{%endraw%}
+
+),
+
+renamed as (
+
+    select
+        my_integer_col
+        , my_bool_col
+
+    from source
+
+)
+
+select * from renamed
+{% endset %}
+
+{{ assert_equal (actual_base_model | trim, expected_base_model | trim) }}
