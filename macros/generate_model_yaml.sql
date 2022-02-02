d7e47f174603523828742db68d3671c9d53d212d
Bob De Schutter <bob.deschutter@digitalswat.be>
2022-02-02 13:34:25 +0100

    Updated generate_model_yaml macro to correctly handle nested bigquery fields

--- macros/generate_model_yaml.sql
+++ macros/generate_model_yaml.sql
@@ -1,3 +1,21 @@
+{% macro generate_column_yaml(column, model_yaml, parent_column_name="") %}
+    {% if parent_column_name %}
+        {% set column_name = parent_column_name ~ "." ~ column.name %}
+    {% else %}
+        {% set column_name = column.name %}
+    {% endif %}
+
+    {% do model_yaml.append('      - name: ' ~ column_name | lower ) %}
+    {% do model_yaml.append('        description: ""') %}
+    {% do model_yaml.append('') %}
+    {% if column.fields|length > 0 %}
+        {% for child_column in column.fields %}
+            {% set model_yaml = generate_column_yaml(child_column, model_yaml, parent_column_name=column_name) %}
+        {% endfor %}
+    {% endif %}
+    {% do return(model_yaml) %}
+{% endmacro %}
+
 {% macro generate_model_yaml(model_name) %}
 
 {% set model_yaml=[] %}
@@ -13,9 +31,7 @@
 {%- set columns = adapter.get_columns_in_relation(relation) -%}
 
 {% for column in columns %}
-    {% do model_yaml.append('      - name: ' ~ column.name | lower ) %}
-    {% do model_yaml.append('        description: ""') %}
-    {% do model_yaml.append('') %}
+    {% set model_yaml = generate_column_yaml(column, model_yaml) %}
 {% endfor %}
 
 {% if execute %}
