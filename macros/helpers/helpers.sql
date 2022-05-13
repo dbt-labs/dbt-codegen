e6a14aa5c45ef430868949f758ffd0ffcb115ead
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2022-05-13 16:18:27 +0200

    Fix typos

--- macros/helpers/helpers.sql
+++ macros/helpers/helpers.sql
@@ -1,4 +1,4 @@
-{# retrieve directly upstream models from a given model #}
+{# retrieve models directly upstream from a given model #}
 {% macro get_model_dependencies(model_name) %}
     {% for node in graph.nodes.values() | selectattr('name', "equalto", model_name) %}
         {{ return(node.depends_on.nodes) }}
@@ -6,7 +6,7 @@
 {% endmacro %}
 
 
-{# add to an input dictionnary entries containing all the column descriptions of a given model #}
+{# add to an input dictionary entries containing all the column descriptions of a given model #}
 {% macro add_model_column_descriptions_to_dict(model_name,dict_with_descriptions={}) %}
     {% for node in graph.nodes.values() | selectattr('name', "equalto", model_name) %}
         {% for col_name, col_values in node.columns.items() %}
@@ -16,7 +16,7 @@
     {{ return(dict_with_descriptions) }}
 {% endmacro %}
 
-{# build a global dictionnary looping through all the direct parents models #}
+{# build a global dictionary looping through all the direct parents models #}
 {# if the same column name exists with different descriptions it is overwritten at each loop #}
 {% macro build_dict_column_descriptions(model_name) %}
     {% if execute %}
