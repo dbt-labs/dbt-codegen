637561578178e0f9db365cec6ba085b4826a9ad8
Grace Goheen <graciegoheen@gmail.com>
2022-06-07 09:36:58 -0400

    enable_case_sensitive_identifier for redshift

--- integration_tests/macros/operations/create_source_table.sql
+++ integration_tests/macros/operations/create_source_table.sql
@@ -1,5 +1,12 @@
 {% macro create_source_table() %}
 
+{% if target.type == "redshift" %} 
+{% set disable_case_sensitive %}
+reset enable_case_sensitive_identifier;
+{% endset %}
+{{ run_query(disable_case_sensitive) }}
+{% endif %}
+
 {% set target_schema=api.Relation.create(
     database=target.database,
     schema="codegen_integration_tests__data_source_schema"
@@ -31,6 +38,12 @@ drop table if exists {{ target_schema }}.codegen_integration_tests__data_source_
 
 {{ run_query(drop_table_sql_case_sensitive) }}
 
+{% if target.type == "redshift" %} 
+{% set enable_case_sensitive %}
+set enable_case_sensitive_identifier to true;
+{% endset %}
+{{ run_query(enable_case_sensitive) }}
+{% endif %}
 
 {% set create_table_sql_case_sensitive %}
 create table {{ target_schema }}.codegen_integration_tests__data_source_table_case_sensitive as (
