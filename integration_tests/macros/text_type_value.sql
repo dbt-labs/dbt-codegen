83831ac59d7cb6da7b39d13b883a2dc6e83f2229
Grigorii Sokolik <grigorii.sokolik@smartly.io>
2022-08-17 13:57:46 +0300

    Fix test_generate_source_all_args for Snowflake

--- integration_tests/macros/text_type_value.sql
+++ integration_tests/macros/text_type_value.sql
@@ -1,6 +1,8 @@
 {%- macro text_type_value(text_length) -%}
 {%- if target.type == "redshift" -%}
 CHARACTER VARYING({{ text_length }})
+{%- elif target.type == "snowflake" -%}
+CHARACTER VARYING(16777216)
 {%- else -%}
 TEXT
 {%- endif -%}
