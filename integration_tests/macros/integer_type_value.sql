3a1267bd1511d7fa2f200d606a7ce1fea515156f
Grigorii Sokolik <grigorii.sokolik@smartly.io>
2022-08-17 14:13:00 +0300

    Fix typo

--- integration_tests/macros/integer_type_value.sql
+++ integration_tests/macros/integer_type_value.sql
@@ -1,7 +1,7 @@
 {%- macro integer_type_value() -%}
 {%- if target.type == "snowflake" -%}
 NUMBER(38,0)
-{%- elif target.type === "bigquery" -%}
+{%- elif target.type == "bigquery" -%}
 INT64
 {%- else -%}
 INTEGER
