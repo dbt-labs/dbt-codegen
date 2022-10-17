4d5a2aa236b4c1c132d32916d0ae44c6da58ac9c
fivetran-joemarkiewicz <joseph.markiewicz@fivetran.com>
2022-10-17 16:25:42 -0500

    final bash script changes

--- bash_scripts/base_model_creation.sh
+++ bash_scripts/base_model_creation.sh
@@ -2,4 +2,4 @@
 
 echo "" > models/stg_$1__$2.sql
 
-dbt run-operation codegen.generate_base_model --args '{"source_name": "'$1'", "table_name": "'$2'"}' | tail -n +3 >> models/$1__$2.sql
+dbt run-operation codegen.generate_base_model --args '{"source_name": "'$1'", "table_name": "'$2'"}' | tail -n +3 >> models/stg_$1__$2.sql
