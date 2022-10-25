c24912ad6df411e2e0f3f462386657128e03b16d
angelica-lastra <angelica.lastra@dbtlabs.com>
2022-10-25 10:44:03 -0700

    Added generate_model_yaml_multiple_models test

---
+++ integration_tests/tests/test_generate_model_yaml_multiple_models.sql
@@ -0,0 +1,30 @@
+{% set actual_model_yaml = codegen.generate_model_yaml(
+    model_name=['data__a_relation','data__b_relation']
+  )
+%}
+
+{% set expected_model_yaml %}
+version: 2
+
+models:
+  - name: data__a_relation
+    description: ""
+    columns:
+      - name: col_a
+        description: ""
+
+      - name: col_b
+        description: ""
+
+  - name: data__b_relation
+    description: ""
+    columns:
+      - name: col_a
+        description: ""
+
+      - name: col_b
+        description: ""
+
+{% endset %}
+
+{{ assert_equal (actual_model_yaml | trim, expected_model_yaml | trim) }}
