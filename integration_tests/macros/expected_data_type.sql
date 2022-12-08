96933f6594a435703591da69dd6f5508152f1f5e
Grigorii Sokolik <grigorii.sokolik@smartly.io>
2022-12-08 13:50:16 +0200

    Dummy fix

--- integration_tests/macros/expected_data_type.sql
+++ integration_tests/macros/expected_data_type.sql
@@ -10,7 +10,7 @@
     {%- set column = api.Column.create(none, data_type) -%}
   {%- endif -%}
   {{ column.dtype }}
-  {%- if column.char_size -%}({{ column.char_size }})
+  {%- if column.string_size() and string_size != 256 -%}({{ column.string_size() }})
   {%- elif column.numeric_precision and column.numeric_scale -%}({{
     column.numeric_precision }},{{ column.numeric_scale }})
   {%- endif -%}
