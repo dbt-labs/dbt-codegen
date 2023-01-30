c6e7a98bcb96192deae145200375e737ac107601
Erkan Celen <erkanncelen@gmail.com>
2023-01-30 13:33:19 +0100

    fixing graph object issues in testing

--- integration_tests/tests/test_generate_model_yaml_using_get_model.sql
+++ integration_tests/tests/test_generate_model_yaml_using_get_model.sql
@@ -1,22 +1,44 @@
+-- depends_on: {{ ref('model_data_a'),ref('model_struct'), }}
+-- depends_on: {{ ref('model_struct') }}
+-- depends_on: {{ ref('model_without_import_ctes') }}
+-- depends_on: {{ ref('model_without_any_ctes') }}
+
+{% if execute %}
 {% set actual_model_yaml = codegen.generate_model_yaml(
-    model_names= codegen.get_models(prefix='data__')
+    model_names = codegen.get_models(prefix='model_')
   )
 %}
+{% endif %}
 
 {% set expected_model_yaml %}
 version: 2
 
 models:
-  - name: data__a_relation
+  - name: model_without_import_ctes
     description: ""
     columns:
-      - name: col_a
+  - name: model_without_any_ctes
+    description: ""
+    columns:
+  - name: model_struct
+    description: ""
+    columns:
+      - name: analytics
         description: ""
 
-      - name: col_b
+      - name: analytics.source
         description: ""
 
-  - name: data__b_relation
+      - name: analytics.medium
+        description: ""
+
+      - name: analytics.source_medium
+        description: ""
+
+      - name: col_x
+        description: ""
+
+  - name: model_data_a
     description: ""
     columns:
       - name: col_a
