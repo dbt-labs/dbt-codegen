55dfb963f75c6cb7e7c3779315702a2740a538aa
Erkan Celen <erkanncelen@gmail.com>
2023-01-30 13:57:42 +0100

    sorting actual list result

--- integration_tests/tests/test_helper_get_models.sql
+++ integration_tests/tests/test_helper_get_models.sql
@@ -4,11 +4,11 @@
 -- depends_on: {{ ref('model_without_any_ctes') }}
 
 {% if execute %}
-{% set actual_list = codegen.get_models(prefix='model_') %}
+{% set actual_list = codegen.get_models(prefix='model_')|sort %}
 {% endif %}
 
 {% set expected_list %}
-['model_without_import_ctes', 'model_without_any_ctes', 'model_struct', 'model_data_a']
+['model_data_a', 'model_struct', 'model_without_any_ctes', 'model_without_import_ctes']
 {% endset %}
 
 {{ assert_equal (actual_list | trim, expected_list | trim) }}
