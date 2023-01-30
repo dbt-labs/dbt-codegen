9c3ca18479730ce6b3e6f8968473e447b6d362de
Erkan Celen <erkanncelen@gmail.com>
2023-01-30 12:07:03 +0100

    integration tests

---
+++ integration_tests/tests/test_generate_model_yaml_using_get_model.sql
@@ -0,0 +1,30 @@
+{% set actual_model_yaml = codegen.generate_model_yaml(
+    model_names= codegen.get_modelsd(prefix='data__')
+  )
+%}
+
+{% set expected_model_yaml %}
+version: 2
+
+models:
+  - name: data__a_relation
+    description: ""
+    columns:
+      - name: col_a
+        description: ""
+
+      - name: col_b
+        description: ""
+
+  - name: data__b_relation
+    description: ""
+    columns:
+      - name: col_a
+        description: ""
+
+      - name: col_b
+        description: ""
+
+{% endset %}
+
+{{ assert_equal (actual_model_yaml | trim, expected_model_yaml | trim) }}
