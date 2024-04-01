9347477a3087cf6f6454e0864a905b8a69be4ed2
Pat Nadolny <patnadolny@gmail.com>
2024-04-01 10:07:22 -0500

    Add case sensitivity to `generate_source` macro (#168)
    * new test for case sensitive, status quo
    
    * assert case sensitive
    
    * implement changes

--- macros/generate_source.sql
+++ macros/generate_source.sql
@@ -13,11 +13,11 @@
 
 {% endmacro %}
 
-{% macro generate_source(schema_name, database_name=target.database, generate_columns=False, include_descriptions=False, include_data_types=True, table_pattern='%', exclude='', name=schema_name, table_names=None, include_database=False, include_schema=False) %}
-    {{ return(adapter.dispatch('generate_source', 'codegen')(schema_name, database_name, generate_columns, include_descriptions, include_data_types, table_pattern, exclude, name, table_names, include_database, include_schema)) }}
+{% macro generate_source(schema_name, database_name=target.database, generate_columns=False, include_descriptions=False, include_data_types=True, table_pattern='%', exclude='', name=schema_name, table_names=None, include_database=False, include_schema=False, case_sensitive_databases=False, case_sensitive_schemas=False, case_sensitive_tables=False, case_sensitive_cols=False) %}
+    {{ return(adapter.dispatch('generate_source', 'codegen')(schema_name, database_name, generate_columns, include_descriptions, include_data_types, table_pattern, exclude, name, table_names, include_database, include_schema, case_sensitive_databases, case_sensitive_schemas, case_sensitive_tables, case_sensitive_cols)) }}
 {% endmacro %}
 
-{% macro default__generate_source(schema_name, database_name, generate_columns, include_descriptions, include_data_types, table_pattern, exclude, name, table_names, include_database, include_schema) %}
+{% macro default__generate_source(schema_name, database_name, generate_columns, include_descriptions, include_data_types, table_pattern, exclude, name, table_names, include_database, include_schema, case_sensitive_databases, case_sensitive_schemas, case_sensitive_tables, case_sensitive_cols) %}
 
 {% set sources_yaml=[] %}
 {% do sources_yaml.append('version: 2') %}
@@ -30,11 +30,11 @@
 {% endif %}
 
 {% if database_name != target.database or include_database %}
-{% do sources_yaml.append('    database: ' ~ database_name | lower) %}
+{% do sources_yaml.append('    database: ' ~ (database_name if case_sensitive_databases else database_name | lower)) %}
 {% endif %}
 
 {% if schema_name != name or include_schema %}
-{% do sources_yaml.append('    schema: ' ~ schema_name | lower) %}
+{% do sources_yaml.append('    schema: ' ~ (schema_name if case_sensitive_schemas else schema_name | lower)) %}
 {% endif %}
 
 {% do sources_yaml.append('    tables:') %}
@@ -46,7 +46,7 @@
 {% endif %}
 
 {% for table in tables %}
-    {% do sources_yaml.append('      - name: ' ~ table | lower ) %}
+    {% do sources_yaml.append('      - name: ' ~ (table if case_sensitive_tables else table | lower) ) %}
     {% if include_descriptions %}
         {% do sources_yaml.append('        description: ""' ) %}
     {% endif %}
@@ -62,7 +62,7 @@
         {% set columns=adapter.get_columns_in_relation(table_relation) %}
 
         {% for column in columns %}
-            {% do sources_yaml.append('          - name: ' ~ column.name | lower ) %}
+            {% do sources_yaml.append('          - name: ' ~ (column.name if case_sensitive_cols else column.name | lower)) %}
             {% if include_data_types %}
                 {% do sources_yaml.append('            data_type: ' ~ codegen.data_type_format_source(column)) %}
             {% endif %}
