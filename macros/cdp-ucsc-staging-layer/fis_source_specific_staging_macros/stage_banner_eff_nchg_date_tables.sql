02f22e95d2c25414769ef96f41057013ac83dc81
insuhpak <ikpak@ucsc.edu>
2024-05-23 10:20:31 -0700

    update fis efft nchg date macro

--- macros/cdp-ucsc-staging-layer/fis_source_specific_staging_macros/stage_banner_eff_nchg_date_tables.sql
+++ macros/cdp-ucsc-staging-layer/fis_source_specific_staging_macros/stage_banner_eff_nchg_date_tables.sql
@@ -1,6 +1,6 @@
-{# dbt run-operation --quiet stg_spcl_fis_eff_type_3_to_type_2 --args '{"source_name": "fis", "table_name": "frrbasi", "alphabetize": true}' #}
+{# dbt run-operation --quiet stage_banner_eff_nchg_date_tables --args '{"source_name": "fis", "table_name": "frrbasi", "alphabetize": true}' #}
 
-{% macro stg_spcl_fis_eff_type_3_to_type_2(source_name, table_name, partition_columns, alphabetize=true) %}
+{% macro stage_banner_eff_nchg_date_tables(source_name, table_name, partition_columns, alphabetize=true) %}
 
 {# GET ALL COLUMNS FROM SOURCE TABLE (this information comes from the database)#}
 {%- set source_relation = source(source_name, table_name) -%}
@@ -17,8 +17,8 @@
 {% set ns = namespace() %} {# Use namespace so ns.eff_col can persist outside of the for loop it is defined in #}
 {%- set ns.eff_col = "" -%}
 {%- set ns.eff_col_renamed_as = "" -%}
+{%- set ns.nchg_col_renamed_as = "" -%}
 {%- set partition_cols = [] -%}
-{%- set partition_cols_renamed_as = [] -%}
 {%- set col_casted_as =  {} -%}
 {%- set col_renamed_as = {} -%}
 {%- set soft_delete_cols = [] -%}
@@ -59,17 +59,14 @@
 {%- set ns.eff_col_renamed_as = ns.eff_col -%}
 {%- endif -%}
 
-{%- for i in partition_cols -%}
-{%- if col_renamed_as[i] -%}
-{%- do partition_cols_renamed_as.append(col_renamed_as[i]) -%}
+{%- set nchg_col = table_name ~ '_nchg_date' -%}
+{%- if col_renamed_as[nchg_col] -%}
+{%- set ns.nchg_col_renamed_as = col_renamed_as[nchg_col] -%}
 {%- else -%}
-{%- do partition_cols_renamed_as.append(i) -%}
+{%- set ns.nchg_col_renamed_as = nchg_col -%}
 {%- endif -%}
-{%- endfor -%}
 
-{%- set stg_spcl_fis_eff_type_3_to_type_2 -%}
-/*  The partition columns are: {{partition_cols}}
-*/
+{%- set stage_banner_eff_nchg_date_tables -%}
 
 with
     source as (
@@ -84,12 +81,8 @@ with
         -- Derive a sequence number for records that share the same effective day
         select
         {%- for i in col_names %}
-            {# (0) KEEP ORIGINAL TIMESTAMP EFF DATE COLUMN AND CAST TO DATE #}
-            {%- if i == ns.eff_col -%}
-            {{i}} as {{i}}_timestamp,
-            cast({{i}} as date) as {{i}},
             {# (1) CAST AND RENAME #}
-            {%- elif col_casted_as[i] and col_renamed_as[i] -%}
+            {%- if col_casted_as[i] and col_renamed_as[i] -%}
             cast({{i}} as {{col_casted_as[i]}}) as {{col_renamed_as[i]}},
             {#- (2) CAST ONLY #}
             {%- elif col_casted_as[i] and not col_renamed_as[i] -%}
@@ -102,37 +95,20 @@ with
             {{i}},
             {%- endif -%}
         {%- endfor %}
-
             row_number() over (
                 partition by
                     {{ partition_cols | join(',\n') | indent(20) }},
                     cast({{ns.eff_col}} as date)
-
                 order by
-                    {{ns.eff_col}} desc,
-                    {{table_name}}_nchg_date desc
-            ) as {{table_name}}_effseq
+                    {{ns.eff_col}}
+            ) as {{table_name}}_effseq,
+            count(*) over (
+                partition by
+                    {{ partition_cols | join(',\n') | indent(20) }},
+                    cast({{ns.eff_col}} as date)
+            ) as {{table_name}}_max_effseq
 
         from source
-    ),
-
-    valid_to as (
-        -- Group the records so that each group belonging to one effdt gets the same valid_to date
-        select
-            {{ partition_cols_renamed_as | join(',\n') | indent(12) }},
-            {{ns.eff_col_renamed_as}},
-            coalesce((lag({{ns.eff_col_renamed_as}}, 1) over (
-                partition by
-                    {{ partition_cols_renamed_as | join(',\n') | indent(20) }}
-                order by {{ns.eff_col_renamed_as}} desc
-            ) - 1), '2099-12-31') as valid_to
-
-        from derive_effseq_and_transform
-        group by
-            {% for i in partition_cols_renamed_as -%}
-                            {{loop.index}},
-            {{loop.index + 1 if loop.last }}
-            {%- endfor %}
     ),
 
     final as (
@@ -140,20 +116,18 @@ with
             derive_effseq_and_transform.*,
 
             -- New Objects
-            derive_effseq_and_transform.{{ns.eff_col_renamed_as}} as valid_from,
-            derive_effseq_and_transform.{{table_name}}_effseq = 1 as is_latter_rcd_of_effdt,
-            case
-                when is_latter_rcd_of_effdt = true then valid_to.valid_to
-                else derive_effseq_and_transform.{{ns.eff_col_renamed_as}}
-            end as valid_to,
+            cast(derive_effseq_and_transform.{{ns.eff_col_renamed_as}} as date) as valid_from,
+            coalesce(cast(derive_effseq_and_transform.{{ns.nchg_col_renamed_as}} as date), '2099-12-31') as valid_to,
+            derive_effseq_and_transform.{{table_name}}_effseq
+            = derive_effseq_and_transform.{{table_name}}_max_effseq as is_max_rcd_of_effdt,
             case
                 when valid_from > {{'{{'}} var("current_date_pst") {{'}}'}} then 'future'
-                when valid_to.valid_to < {{'{{'}} var("current_date_pst") {{'}}'}} then 'past'
-                when valid_to.valid_to >= {{'{{'}} var("current_date_pst") {{'}}'}}
-                    and is_latter_rcd_of_effdt = true
+                when valid_to < {{'{{'}} var("current_date_pst") {{'}}'}} then 'past'
+                when valid_to >= {{'{{'}} var("current_date_pst") {{'}}'}}
+                    and is_max_rcd_of_effdt = true
                     then 'current'
-                when valid_to.valid_to >= {{'{{'}} var("current_date_pst") {{'}}'}}
-                    and is_latter_rcd_of_effdt = false
+                when valid_to >= {{'{{'}} var("current_date_pst") {{'}}'}}
+                    and is_max_rcd_of_effdt = false
                     then 'past'
             end as current_record_desc,
             case
@@ -162,21 +136,16 @@ with
             end as is_current_record
 
         from derive_effseq_and_transform
-
-        left outer join valid_to
-            {% for i in partition_cols_renamed_as -%}
-            {{'on' if loop.first else '\n                and'}} derive_effseq_and_transform.{{i}} = valid_to.{{i}}{% endfor %}
-                and derive_effseq_and_transform.{{ns.eff_col_renamed_as}} = valid_to.{{ns.eff_col_renamed_as}}
     )
 
 select * from final
-where is_latter_rcd_of_effdt = true
+
 {%- endset -%}
 
 
 {%- if execute -%}
-    {{ print(stg_spcl_fis_eff_type_3_to_type_2) }}
-    {%- do return(stg_spcl_fis_eff_type_3_to_type_2) -%}
+    {{ print(stage_banner_eff_nchg_date_tables) }}
+    {%- do return(stage_banner_eff_nchg_date_tables) -%}
 {%- endif -%}
 
 {%- endmacro -%}
