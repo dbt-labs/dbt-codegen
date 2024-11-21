f47dc53541156a2445ffeb8faa16460c25356bad
Doug Beatty <doug.beatty@dbtlabs.com>
2024-11-21 07:20:45 -0700

    Try removing Redshift-specific logic

--- integration_tests/macros/operations/create_source_table.sql
+++ integration_tests/macros/operations/create_source_table.sql
@@ -1,12 +1,5 @@
 {% macro create_source_table() %}
 
-{% if target.type == "redshift" %} 
-{% set disable_case_sensitive %}
-reset enable_case_sensitive_identifier;
-{% endset %}
-{{ run_query(disable_case_sensitive) }}
-{% endif %}
-
 {% set target_schema=api.Relation.create(
     database=target.database,
     schema="codegen_integration_tests__data_source_schema"
@@ -37,13 +30,6 @@ drop table if exists {{ target_schema }}.codegen_integration_tests__data_source_
 {% endset %}
 
 {{ run_query(drop_table_sql_case_sensitive) }}
-
-{% if target.type == "redshift" %} 
-{% set enable_case_sensitive %}
-set enable_case_sensitive_identifier to true;
-{% endset %}
-{{ run_query(enable_case_sensitive) }}
-{% endif %}
 
 {% set create_table_sql_case_sensitive %}
 create table {{ target_schema }}.codegen_integration_tests__data_source_table_case_sensitive as (
