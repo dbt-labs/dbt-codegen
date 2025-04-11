174a7a192c0832e9ab89351762c5e38505ec6187
Fiona Zhao <fiona.zhao@shopify.com>
2025-04-10 21:33:38 -0400

    Fix expected data type in integration_test

--- integration_tests/macros/text_type_value.sql
+++ integration_tests/macros/text_type_value.sql
@@ -1,6 +1,6 @@
 {%- macro text_type_value(case_sensitive=False) -%}
 {%- if target.type == "redshift"-%}
-    text
+    character varying
 {%- elif target.type == "snowflake" -%}
     {%- if case_sensitive -%}VARCHAR{%- else -%}varchar{%- endif -%}
 {%- elif target.type == "bigquery" -%}
