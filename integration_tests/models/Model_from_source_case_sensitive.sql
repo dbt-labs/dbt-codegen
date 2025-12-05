23c4b25700948868d400347e9203a9e8708015ac
Jiri Merunka <jiri.merunka@seznam.cz>
2025-12-05 12:25:29 +0100

    feat: add SQL file for case-sensitive model testing

--- integration_tests/models/Model_from_source_case_sensitive.sql
+++ integration_tests/models/Model_from_source_case_sensitive.sql
@@ -1,4 +1,4 @@
 select
     *
-from {{ source('codegen_integration_tests__data_source_schema', 'codegen_integration_tests__data_source_table') }}
+from {{ source('codegen_integration_tests__data_source_schema', 'codegen_integration_tests__data_source_table_case_sensitive') }}
 
