978c366bd17cd515fbba4cc7c3f73b2ca3ff42a2
Arijit Kumar Roy <43462564+arijitroy003@users.noreply.github.com>
2026-07-28 16:11:17 +0530

    fix: quote shell variables to prevent injection (#271)

--- bash_scripts/base_model_creation.sh
+++ bash_scripts/base_model_creation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "" > models/stg_$1__$2.sql
+echo "" > models/stg_"$1"__"$2".sql
 
-dbt --quiet run-operation codegen.generate_base_model --args '{"source_name": "'$1'", "table_name": "'$2'"}' | tail -n +3 >> models/stg_$1__$2.sql
+dbt --quiet run-operation codegen.generate_base_model --args '{"source_name": "'"$1"'", "table_name": "'"$2"'"}' | tail -n +3 >> models/stg_"$1"__"$2".sql
